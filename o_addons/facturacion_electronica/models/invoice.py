8d6ead467abac464189a7110ad819eef603be3f1
Christian <christian.bravo@knd.pe>
2019-02-15 12:21:07 -0500

    print

--- o_addons/facturacion_electronica/models/invoice.py
+++ o_addons/facturacion_electronica/models/invoice.py
@@ -368,6 +368,8 @@ class invoice(models.Model):
     # @api.model
     def _envio_masivo(self):
         facturas = self.search([['codigoretorno', '=', False], ['state', 'in', ['open', 'paid']], ['journal_id.invoice_type_code_id', '=', '01']])
+        print('Facturas a consultar:')
+        print(facturas)
         for f in facturas:
             FacturaObject = Factura()
             EnvioXML = FacturaObject.getStatus(
